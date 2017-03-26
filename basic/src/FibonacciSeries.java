8e217237c061e2ca84adef05c0d7d66c01708844
sumitiisegit <sumit.iise@gmail.com>
2017-03-26 17:40:35 +0530

    basic

---
+++ basic/src/FibonacciSeries.java
@@ -0,0 +1,8 @@
+/**
+ * Created by shilpa on 26-03-2017.
+ */
+public class FibonacciSeries {
+    public int a=1;
+
+    main
+}
---
+++ basic/src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by shilpa on 26-03-2017.
+ */
+public class HelloWorld {
+
+    public static void main(String args[]) {
+        System.out.print("Hello world");
+    }}
